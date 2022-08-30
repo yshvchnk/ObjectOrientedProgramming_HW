af660bc5060f1bc098a3864e14287c39369620fa
Yurii Shevchenko <102354607+yshvchnk@users.noreply.github.com>
2022-08-30 21:55:30 +0300

    Add files via upload
    add 0 practical work for OOP course_Yurii Shevchenko_222/3

---
+++ practice00/MyJavaProject/src/Main/Main.java
@@ -0,0 +1,19 @@
+/*Yurii Shevchenko,222/3 group*/
+
+package Main;
+
+public class Main {
+
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+
+}
